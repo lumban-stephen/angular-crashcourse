1e0f691b5de676ac49e8157a80bcc521902be75a
stephenlumban <stephenl@n-compass.biz>
2024-10-29 13:28:29 +0800

    finished pagination

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,11 +3,12 @@ import { ProductsService } from '../services/product.service';
 import { ProductComponent } from "../components/product/product.component";
 import { Product, Products } from '../../types';
 import { CommonModule } from '@angular/common';
+import { PaginatorModule } from 'primeng/paginator';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ProductComponent, CommonModule],
+  imports: [ProductComponent, CommonModule, PaginatorModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
@@ -18,13 +19,26 @@ export class HomeComponent {
   ) {}
 
   products: Product[] = [];
+  rows: number = 5;
+  totalRecords: number = 0;
+
   onProductOutput(product: Product) {
     console.log(product, "Output")
   }
-  ngOnInit() {
+
+  onPageChange(event: any) {
+    this.fetchProducts(event.page, event.rows);
+  } 
+
+  fetchProducts(page: number, perPage: number) {
     this.productsService.getProducts('http://localhost:3000/clothes', {page: 0, perPage: 5})
     .subscribe((products: Products) => {
       this.products = products.items;
+      this.totalRecords = products.total;
     })
+  }
+
+  ngOnInit() {
+    this.fetchProducts(0,5);
   }
 }
